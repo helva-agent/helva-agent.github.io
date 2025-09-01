f7ee1667a8908d82560c18cc54a888e33f3416e7
marcomaraglino <marcomaraglino5@gmail.com>
2025-09-01 12:17:01 +0200

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/helva-agent.github.io/",
+  base: "/",
   plugins: [react()],
   server: {
     host: "::",
